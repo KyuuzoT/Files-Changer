0ebe92679031e3e3d1c95b9b1b2973615b144323
Roman Okatov (KyuuzoT) <okatov.r@gmail.com>
2026-04-30 14:54:06 +0300

    Changing the form's design to accomodate localization efforts.

--- FilesChanger/View/FilesChanger.Designer.cs
+++ FilesChanger/View/FilesChanger.Designer.cs
@@ -29,158 +29,196 @@
         private void InitializeComponent()
         {
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FilesChanger));
-            this.pbBar = new System.Windows.Forms.ProgressBar();
-            this.FilesListView = new System.Windows.Forms.CheckedListBox();
-            this.btnPath = new System.Windows.Forms.Button();
-            this.btnStart = new System.Windows.Forms.Button();
-            this.CurrentFile = new System.Windows.Forms.Label();
-            this.btnCheckAll = new System.Windows.Forms.Button();
-            this.cbRename = new System.Windows.Forms.CheckBox();
-            this.groupBox1 = new System.Windows.Forms.GroupBox();
-            this.rbTopDirectory = new System.Windows.Forms.RadioButton();
-            this.rbTopChildDirectories = new System.Windows.Forms.RadioButton();
-            this.groupBox1.SuspendLayout();
-            this.SuspendLayout();
+            pbBar = new ProgressBar();
+            FilesListView = new CheckedListBox();
+            btnPath = new Button();
+            btnStart = new Button();
+            CurrentFile = new Label();
+            btnCheckAll = new Button();
+            cbRename = new CheckBox();
+            groupBox1 = new GroupBox();
+            rbTopChildDirectories = new RadioButton();
+            rbTopDirectory = new RadioButton();
+            gbLanguageChange = new GroupBox();
+            bLanguageEn = new Button();
+            bLanguageRu = new Button();
+            groupBox1.SuspendLayout();
+            gbLanguageChange.SuspendLayout();
+            SuspendLayout();
             // 
             // pbBar
             // 
-            this.pbBar.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pbBar.Location = new System.Drawing.Point(10, 337);
-            this.pbBar.Margin = new System.Windows.Forms.Padding(2);
-            this.pbBar.Name = "pbBar";
-            this.pbBar.Size = new System.Drawing.Size(581, 19);
-            this.pbBar.TabIndex = 0;
+            pbBar.Anchor = AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            pbBar.Location = new Point(20, 778);
+            pbBar.Margin = new Padding(4, 5, 4, 5);
+            pbBar.Name = "pbBar";
+            pbBar.Size = new Size(1162, 44);
+            pbBar.TabIndex = 0;
             // 
             // FilesListView
             // 
-            this.FilesListView.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.FilesListView.CheckOnClick = true;
-            this.FilesListView.FormattingEnabled = true;
-            this.FilesListView.Location = new System.Drawing.Point(275, 11);
-            this.FilesListView.Margin = new System.Windows.Forms.Padding(2);
-            this.FilesListView.Name = "FilesListView";
-            this.FilesListView.Size = new System.Drawing.Size(317, 274);
-            this.FilesListView.TabIndex = 1;
+            FilesListView.Anchor = AnchorStyles.Top | AnchorStyles.Bottom | AnchorStyles.Left | AnchorStyles.Right;
+            FilesListView.CheckOnClick = true;
+            FilesListView.FormattingEnabled = true;
+            FilesListView.Location = new Point(550, 89);
+            FilesListView.Margin = new Padding(4, 5, 4, 5);
+            FilesListView.Name = "FilesListView";
+            FilesListView.Size = new Size(630, 548);
+            FilesListView.TabIndex = 1;
             // 
             // btnPath
             // 
-            this.btnPath.Location = new System.Drawing.Point(10, 39);
-            this.btnPath.Margin = new System.Windows.Forms.Padding(2);
-            this.btnPath.Name = "btnPath";
-            this.btnPath.Size = new System.Drawing.Size(141, 40);
-            this.btnPath.TabIndex = 2;
-            this.btnPath.Text = "Открыть папку";
-            this.btnPath.UseVisualStyleBackColor = true;
-            this.btnPath.Click += new System.EventHandler(this.btnPath_Click);
+            btnPath.Location = new Point(20, 90);
+            btnPath.Margin = new Padding(4, 5, 4, 5);
+            btnPath.Name = "btnPath";
+            btnPath.Size = new Size(282, 92);
+            btnPath.TabIndex = 2;
+            btnPath.Text = "Открыть папку";
+            btnPath.UseVisualStyleBackColor = true;
+            btnPath.Click += btnPath_Click;
             // 
             // btnStart
             // 
-            this.btnStart.Font = new System.Drawing.Font("Anime Ace v02", 10.2F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.btnStart.ForeColor = System.Drawing.Color.ForestGreen;
-            this.btnStart.Location = new System.Drawing.Point(10, 102);
-            this.btnStart.Margin = new System.Windows.Forms.Padding(2);
-            this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(141, 40);
-            this.btnStart.TabIndex = 3;
-            this.btnStart.Text = "Запустить";
-            this.btnStart.UseVisualStyleBackColor = true;
-            this.btnStart.Click += new System.EventHandler(this.button2_Click);
+            btnStart.Font = new Font("Microsoft Sans Serif", 10.2F, FontStyle.Italic, GraphicsUnit.Point, 204);
+            btnStart.ForeColor = Color.ForestGreen;
+            btnStart.Location = new Point(20, 235);
+            btnStart.Margin = new Padding(4, 5, 4, 5);
+            btnStart.Name = "btnStart";
+            btnStart.Size = new Size(282, 92);
+            btnStart.TabIndex = 3;
+            btnStart.Text = "Запустить";
+            btnStart.UseVisualStyleBackColor = true;
+            btnStart.Click += button2_Click;
             // 
             // CurrentFile
             // 
-            this.CurrentFile.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
-            this.CurrentFile.AutoSize = true;
-            this.CurrentFile.Location = new System.Drawing.Point(10, 318);
-            this.CurrentFile.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
-            this.CurrentFile.Name = "CurrentFile";
-            this.CurrentFile.Size = new System.Drawing.Size(56, 13);
-            this.CurrentFile.TabIndex = 4;
-            this.CurrentFile.Text = "Прогресс";
+            CurrentFile.Anchor = AnchorStyles.Bottom | AnchorStyles.Left;
+            CurrentFile.AutoSize = true;
+            CurrentFile.Location = new Point(20, 734);
+            CurrentFile.Margin = new Padding(4, 0, 4, 0);
+            CurrentFile.Name = "CurrentFile";
+            CurrentFile.Size = new Size(103, 30);
+            CurrentFile.TabIndex = 4;
+            CurrentFile.Text = "Прогресс";
             // 
             // btnCheckAll
             // 
-            this.btnCheckAll.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
-            this.btnCheckAll.Location = new System.Drawing.Point(275, 291);
-            this.btnCheckAll.Name = "btnCheckAll";
-            this.btnCheckAll.Size = new System.Drawing.Size(99, 28);
-            this.btnCheckAll.TabIndex = 5;
-            this.btnCheckAll.Text = "Выбрать все";
-            this.btnCheckAll.UseVisualStyleBackColor = true;
-            this.btnCheckAll.Click += new System.EventHandler(this.btnCheckAll_Click);
+            btnCheckAll.Anchor = AnchorStyles.Bottom | AnchorStyles.Right;
+            btnCheckAll.Location = new Point(550, 662);
+            btnCheckAll.Margin = new Padding(6, 7, 6, 7);
+            btnCheckAll.Name = "btnCheckAll";
+            btnCheckAll.Size = new Size(198, 65);
+            btnCheckAll.TabIndex = 5;
+            btnCheckAll.Text = "Выбрать все";
+            btnCheckAll.UseVisualStyleBackColor = true;
+            btnCheckAll.Click += btnCheckAll_Click;
             // 
             // cbRename
             // 
-            this.cbRename.AutoSize = true;
-            this.cbRename.Checked = true;
-            this.cbRename.CheckState = System.Windows.Forms.CheckState.Checked;
-            this.cbRename.Location = new System.Drawing.Point(6, 19);
-            this.cbRename.Name = "cbRename";
-            this.cbRename.Size = new System.Drawing.Size(164, 17);
-            this.cbRename.TabIndex = 6;
-            this.cbRename.Text = "Включить переименование";
-            this.cbRename.UseVisualStyleBackColor = true;
-            this.cbRename.MouseDown += new System.Windows.Forms.MouseEventHandler(this.cbRename_MouseDown);
+            cbRename.AutoSize = true;
+            cbRename.Checked = true;
+            cbRename.CheckState = CheckState.Checked;
+            cbRename.Location = new Point(12, 44);
+            cbRename.Margin = new Padding(6, 7, 6, 7);
+            cbRename.Name = "cbRename";
+            cbRename.Size = new Size(304, 34);
+            cbRename.TabIndex = 6;
+            cbRename.Text = "Включить переименование";
+            cbRename.UseVisualStyleBackColor = true;
+            cbRename.MouseDown += cbRename_MouseDown;
             // 
             // groupBox1
             // 
-            this.groupBox1.Controls.Add(this.rbTopChildDirectories);
-            this.groupBox1.Controls.Add(this.rbTopDirectory);
-            this.groupBox1.Controls.Add(this.cbRename);
-            this.groupBox1.Location = new System.Drawing.Point(10, 162);
-            this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(259, 153);
-            this.groupBox1.TabIndex = 7;
-            this.groupBox1.TabStop = false;
-            this.groupBox1.Text = "Опции";
-            // 
-            // rbTopDirectory
-            // 
-            this.rbTopDirectory.AutoSize = true;
-            this.rbTopDirectory.Location = new System.Drawing.Point(6, 43);
-            this.rbTopDirectory.Name = "rbTopDirectory";
-            this.rbTopDirectory.Size = new System.Drawing.Size(140, 17);
-            this.rbTopDirectory.TabIndex = 7;
-            this.rbTopDirectory.TabStop = true;
-            this.rbTopDirectory.Text = "Родительский каталог";
-            this.rbTopDirectory.UseVisualStyleBackColor = true;
-            this.rbTopDirectory.CheckedChanged += new System.EventHandler(this.rbTopDirectory_CheckedChanged);
+            groupBox1.Controls.Add(rbTopChildDirectories);
+            groupBox1.Controls.Add(rbTopDirectory);
+            groupBox1.Controls.Add(cbRename);
+            groupBox1.Location = new Point(20, 374);
+            groupBox1.Margin = new Padding(6, 7, 6, 7);
+            groupBox1.Name = "groupBox1";
+            groupBox1.Padding = new Padding(6, 7, 6, 7);
+            groupBox1.Size = new Size(518, 353);
+            groupBox1.TabIndex = 7;
+            groupBox1.TabStop = false;
+            groupBox1.Text = "Опции";
             // 
             // rbTopChildDirectories
             // 
-            this.rbTopChildDirectories.AutoSize = true;
-            this.rbTopChildDirectories.Location = new System.Drawing.Point(6, 67);
-            this.rbTopChildDirectories.Name = "rbTopChildDirectories";
-            this.rbTopChildDirectories.Size = new System.Drawing.Size(205, 17);
-            this.rbTopChildDirectories.TabIndex = 8;
-            this.rbTopChildDirectories.TabStop = true;
-            this.rbTopChildDirectories.Text = "Родительский и дочерний каталоги";
-            this.rbTopChildDirectories.UseVisualStyleBackColor = true;
-            this.rbTopChildDirectories.CheckedChanged += new System.EventHandler(this.rbTopChildDirectories_CheckedChanged);
+            rbTopChildDirectories.AutoSize = true;
+            rbTopChildDirectories.Location = new Point(12, 155);
+            rbTopChildDirectories.Margin = new Padding(6, 7, 6, 7);
+            rbTopChildDirectories.Name = "rbTopChildDirectories";
+            rbTopChildDirectories.Size = new Size(379, 34);
+            rbTopChildDirectories.TabIndex = 8;
+            rbTopChildDirectories.TabStop = true;
+            rbTopChildDirectories.Text = "Родительский и дочерний каталоги";
+            rbTopChildDirectories.UseVisualStyleBackColor = true;
+            rbTopChildDirectories.CheckedChanged += rbTopChildDirectories_CheckedChanged;
+            // 
+            // rbTopDirectory
+            // 
+            rbTopDirectory.AutoSize = true;
+            rbTopDirectory.Location = new Point(12, 99);
+            rbTopDirectory.Margin = new Padding(6, 7, 6, 7);
+            rbTopDirectory.Name = "rbTopDirectory";
+            rbTopDirectory.Size = new Size(249, 34);
+            rbTopDirectory.TabIndex = 7;
+            rbTopDirectory.TabStop = true;
+            rbTopDirectory.Text = "Родительский каталог";
+            rbTopDirectory.UseVisualStyleBackColor = true;
+            rbTopDirectory.CheckedChanged += rbTopDirectory_CheckedChanged;
+            // 
+            // gbLanguageChange
+            // 
+            gbLanguageChange.Controls.Add(bLanguageRu);
+            gbLanguageChange.Controls.Add(bLanguageEn);
+            gbLanguageChange.Location = new Point(1030, 0);
+            gbLanguageChange.Name = "gbLanguageChange";
+            gbLanguageChange.Size = new Size(150, 81);
+            gbLanguageChange.TabIndex = 8;
+            gbLanguageChange.TabStop = false;
+            gbLanguageChange.Text = "Language";
+            // 
+            // bLanguageEn
+            // 
+            bLanguageEn.Location = new Point(14, 35);
+            bLanguageEn.Name = "bLanguageEn";
+            bLanguageEn.Size = new Size(58, 40);
+            bLanguageEn.TabIndex = 0;
+            bLanguageEn.Text = "En";
+            bLanguageEn.UseVisualStyleBackColor = true;
+            // 
+            // bLanguageRu
+            // 
+            bLanguageRu.Location = new Point(78, 35);
+            bLanguageRu.Name = "bLanguageRu";
+            bLanguageRu.Size = new Size(58, 40);
+            bLanguageRu.TabIndex = 1;
+            bLanguageRu.Text = "Ru";
+            bLanguageRu.UseVisualStyleBackColor = true;
             // 
             // FilesChanger
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
-            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(600, 366);
-            this.Controls.Add(this.groupBox1);
-            this.Controls.Add(this.btnCheckAll);
-            this.Controls.Add(this.CurrentFile);
-            this.Controls.Add(this.btnStart);
-            this.Controls.Add(this.btnPath);
-            this.Controls.Add(this.FilesListView);
-            this.Controls.Add(this.pbBar);
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.Margin = new System.Windows.Forms.Padding(2);
-            this.MinimumSize = new System.Drawing.Size(616, 405);
-            this.Name = "FilesChanger";
-            this.Text = "Kyuuzo\'s File Changer";
-            this.groupBox1.ResumeLayout(false);
-            this.groupBox1.PerformLayout();
-            this.ResumeLayout(false);
-            this.PerformLayout();
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1200, 845);
+            Controls.Add(gbLanguageChange);
+            Controls.Add(groupBox1);
+            Controls.Add(btnCheckAll);
+            Controls.Add(CurrentFile);
+            Controls.Add(btnStart);
+            Controls.Add(btnPath);
+            Controls.Add(FilesListView);
+            Controls.Add(pbBar);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            Margin = new Padding(4, 5, 4, 5);
+            MinimumSize = new Size(1208, 851);
+            Name = "FilesChanger";
+            Text = "Kyuuzo's File Changer";
+            groupBox1.ResumeLayout(false);
+            groupBox1.PerformLayout();
+            gbLanguageChange.ResumeLayout(false);
+            ResumeLayout(false);
+            PerformLayout();
 
         }
 
@@ -196,6 +234,9 @@
         private System.Windows.Forms.GroupBox groupBox1;
         private System.Windows.Forms.RadioButton rbTopChildDirectories;
         private System.Windows.Forms.RadioButton rbTopDirectory;
+        private GroupBox gbLanguageChange;
+        private Button bLanguageRu;
+        private Button bLanguageEn;
     }
 }
 
