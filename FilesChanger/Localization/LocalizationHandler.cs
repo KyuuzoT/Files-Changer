a7ea64f67b1d5e7a5cbe9242c36f6e26da0bc5b3
Roman Okatov (KyuuzoT) <okatov.r@gmail.com>
2026-05-01 19:22:09 +0300

    Adding localization handler, trying to add json files as resources (failing at the moment)

---
+++ FilesChanger/Localization/LocalizationHandler.cs
@@ -0,0 +1,56 @@
+﻿using FilesChanger.Localization.Models;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FilesChanger.Localization
+{
+    internal class LocalizationHandler
+    {
+        private readonly AvailableLanguages currentLanguage;
+
+        private LocalizationHandler()
+        {
+            currentLanguage = AvailableLanguages.English;
+        }
+
+        public LocalizationHandler(string language)
+        {
+            currentLanguage = language.ToLower() switch
+            {
+                "russian" or "ru" or "ru-ru" => AvailableLanguages.Russian,
+                _ => AvailableLanguages.English,
+            };
+        }
+
+        public LocalizationHandler(AvailableLanguages language)
+        {
+            currentLanguage = language;
+        }
+
+        public IEnumerable<LocalizedStringModel> GetLocalizedStrings()
+        {
+            var model = GetLocalizationModelFromJson();
+
+            var currentLocale = currentLanguage switch
+            {
+                AvailableLanguages.Russian => model.RussianLocale,
+                AvailableLanguages.English => model.EnglishLocale,
+                _ => model.EnglishLocale
+            } 
+            ?? throw new Exception("Application was unable to locate language.");
+
+            return currentLocale.LocalizedStrings ?? throw new Exception("Current localization contains no language-specific strings.");
+        }
+
+        private LocalizationModel GetLocalizationModelFromJson()
+        {
+            string json = string.Empty;
+            return JsonConvert.DeserializeObject<LocalizationModel>(json);
+        }
+
+    }
+}
